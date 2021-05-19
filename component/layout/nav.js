38941a2432b2340aef0e4dbc9a70996effcdfe40
changiojen1 <funfun8811@yahoo.com.tw>
2021-05-19 20:41:04 +0800

    Initial commit

---
+++ component/layout/nav.js
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import { useState } from 'react'
+import styles from './nav.module.scss'
+import Image from 'next/image'
+
+const nav = () => {
+    const [navLinkAct,setNavLinkAct] = useState(false);
+    const toggle = () =>{
+        setNavLinkAct(!navLinkAct)
+    }
+    
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.logo}> 
+                <a href="/"><Image src="/favicon.svg" width={40} height={18}/>Crown Clothing</a>
+            </div>
+            <a href="#" className={styles.toggleBtn} onClick={toggle}>
+                <span className={styles.line}></span>
+                <span className={styles.line}></span>
+                <span className={styles.line}></span>
+            </a>
+            <div className={`${styles.navbar_links} ${navLinkAct?styles.active:''}`}>
+            {/* <div className="navbar_links active"> */}
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/allproducts">All Products</Link></li>
+                    <li><Link href="/overview">Overview</Link></li>
+                    <li>Category
+                        <ul className={styles.dropdown}>
+                            <li>1</li>
+                            <li>1</li>
+                            <li>1</li>
+                            <li>1</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default nav
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Layout from '../component/layout/layout'
+
+export default function Home() {
+  return (
+    <Layout>
+    </Layout>
+  )
+}
